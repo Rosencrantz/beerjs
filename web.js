30c572d31543c60d664bf9acc7e4c23d6314ba1d
Steve Haffenden <steve.haffenden@gmail.com>
2012-08-18 16:09:28 +1000

    sending the index file

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.sendFile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+/*
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express.createServer();
+
+app.get('/', function(request, response) {
+    response.sendfile(__dirname + '/index.html');
+}).configure(function() {
+    app.use('/graphics', express.static(__dirname + '/graphics'));
+	app.use('/stylesheets', express.static(__dirname + '/stylesheets'));
+	app.use('/scripts', express.static(__dirname + '/scripts'));
+}).listen(port);
+*/
