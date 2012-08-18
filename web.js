f58979227e707a9fedf3e5a1531aba2c075b1c01
Steve Haffenden <steve.haffenden@gmail.com>
2012-08-18 16:25:19 +1000

    sorting out resources

--- web.js
+++ web.js
@@ -3,6 +3,12 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.sendfile(__dirname + '/index.html');
+})
+
+app.configure(function () {
+    app.use('/graphics', express.static(__dirname + '/graphics'));
+	app.use('/stylesheets', express.static(__dirname + '/stylesheets'));
+	app.use('/scripts', express.static(__dirname + '/scripts'));
 });
 
 var port = process.env.PORT || 5000;
