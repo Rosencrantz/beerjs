6ad9a285353a66b0f30420f70e7446fa83f7651b
Steve Haffenden <steve.haffenden@gmail.com>
2012-08-18 17:26:35 +1000

    tracking

--- web.js
+++ web.js
@@ -15,17 +15,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-/*
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express.createServer();
-
-app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
-}).configure(function() {
-    app.use('/graphics', express.static(__dirname + '/graphics'));
-	app.use('/stylesheets', express.static(__dirname + '/stylesheets'));
-	app.use('/scripts', express.static(__dirname + '/scripts'));
-}).listen(port);
-*/
